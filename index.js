a333f6250adb9acbd923ae1659a11a46249fc550
Alexandre Paiva <alexandredba510@gmail.com>
2024-05-02 11:03:26 -0300

    Alterações na função para exibir

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Construção de uma classe:
-class heroiDoJogo{
+class heroiDoJogo {
 	constructor(name, age, type){
 		this.name = name
 		this.age = age
@@ -9,12 +9,23 @@ class heroiDoJogo{
 //Função de imprimir 
 imprimir() {
 	console.log(` Nosso herói ${this.name} tem ${this.age} anos e é um ${this.type}`);
+	if(this.type === "mago"){
+		console.log(`O ${this.type} arrancou {ataque} de XP usando magia`);
+		}
+	else if (this.type === "guerreiro"){
+		console.log(`O ${this.type} arrancou {ataque} de XP usando sua espada`);
+		}
+	else if (this.type === "monge"){
+		console.log(`O ${this.type} arrancou {ataque} de XP usando artes marciais`);
+		}
+		else if (this.type === "ninja"){
+			console.log(`O ${this.type} arrancou {ataque} de XP usando sua shuriken`);
+			}
 	}
-}
-
+};
 
 //Instaciar a class
-let heroi01 = new heroiDoJogo("José", "28", "Guerreiro");
+let heroi01 = new heroiDoJogo("José", "28", "monge");
 
 
 //Instaciar a função Imprimir 
